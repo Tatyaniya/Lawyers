69c015b594077f1ef19a2921fe3da3995e60e07a
Tatyaniya <tatyaniya@bigmir.net>
2019-06-09 00:33:06 +0300

    click

--- js/main.js
+++ js/main.js
@@ -9,6 +9,36 @@ $(function () {
           }, 500, function() {
           });
     });*/
+    
+    // modal company
+
+    $('#add-company').click(function(){
+        $( ".company-modal" ).css('display', 'block');
+    });
+
+    $('.company-modal__save').click(function(){
+        $( ".company-modal" ).css('display', 'none');
+    });
+
+    // modal doing
+
+    $('#add-doing').click(function(){
+        $( ".doing-modal" ).css('display', 'block');
+    });
+
+    $('.doing-modal__save').click(function(){
+        $( ".doing-modal" ).css('display', 'none');
+    });
+
+    // choice
+
+    $('.choice').click(function(){
+        console.log('.choice');
+        $('.choice__toogle').toggleClass('choice__toogle--name');
+    });
+
+
+    
   
 
 });
